0ccb606b5c5fcdb206a4f6e9f108c89ce004fd6c
ATRI SAXENA <30728764+CoderAtri@users.noreply.github.com>
2017-10-10 19:57:34 +0530

    Update vignear_cipher.py

--- Ciphers/Vignear Cipher/vignear_cipher.py
+++ Ciphers/Vignear Cipher/vignear_cipher.py
@@ -1,13 +1,18 @@
 strg=str(input("Enter String"))
+strg=str.lower(strg)
+strg=strg.replace(" ","")
 key=str(input("Enter Key"))
+key=str.lower(key)
 i=0
+key_len=len(key)
 for c in strg:
     asci=ord(c)
-    if asci>=97 & asci<=122:
+    if asci>=97 and asci<=122:
         asci=asci+(ord(key[i])-97)
     if asci>122:
         asci=asci%122 +96
-    print(chr(asci),end='')
-    i=i+1
+    #asci=asci+32
+    print(chr(asci-32),end='')
+    i=(i+1)%key_len
     if i>=len(key):
         i=0
